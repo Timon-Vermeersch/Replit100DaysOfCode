07626b2807785f0e295e29baababd4b0d550f19f
YOLOTANKERSWAG2023 <timon.vermeersch@hotmail.com>
2023-11-09 21:01:25 +0100

    tweeterP1

--- 61/61_2.py
+++ 61/61_2.py
@@ -1,28 +1,51 @@
-import sqlite3 as sq
+import sqlite3 as sq,datetime
+##obj = 1 person twitter ww
+#1 add tweets = datetime.datetime.now() = key
+
+#2 view tweets timestamp -  show in reverse chrono - 10per
+
 
 local_db_path = "61\mydatabase.db"
 connection = sq.connect(local_db_path)
 db_cursor = connection.cursor()
 
-dude = (34, "xdxp", 45)  # Removed the extra backslash
-
 sql = """INSERT INTO students(id, name, age)
 VALUES(?, ?, ?)"""
 
-# create_table_sql = '''
-# CREATE TABLE IF NOT EXISTS students (
-#     id INTEGER,
-#     name TEXT,
-#     age INTEGER
-# );
-# '''
-
-# rwar
-#db_cursor.execute(create_table_sql)
 select_sql = "SELECT * FROM students "
 db_cursor.execute(select_sql) 
 
-db_cursor.execute(sql,dude)
 
-connection.commit()
-connection.close()
+
+
+
+def addTweet():
+    print()
+    tweet = input("uw tweet: ")
+
+    tijd = datetime.datetime.now()
+    timekey = (f"{tijd.year}{tijd.month}{tijd.day}{tijd.hour}{tijd.minute}{tijd.second}")
+
+    dude = (timekey, tweet, tijd) 
+    db_cursor.execute(sql,dude)
+
+    connection.commit()
+    connection.close()
+    
+    return tweet
+def viewTweets():
+    print()
+
+
+while True:
+    menu = input("1 for add tweet\n2 for show tweets\n>")
+    if menu == "1":
+        tweet = addTweet()
+        
+        
+
+        
+        
+    if menu == "2":
+        viewTweets()
+
--- 64.py
+++ 64.py
@@ -1,1 +1,20 @@
-###moneymoneyomweiosfjnsioejfm
+import time
+import numpy as np
+
+N = 4069
+
+if __name__ == "__main__":
+    A = np.random.randn(N,N).astype(np.float32)
+    B = np.random.randn(N,N).astype(np.float32)\
+    
+    flop = N*N*N*2
+    print(f"{flop / 1e9:.2f}Gflop")
+
+
+
+    st = time.monotonic()
+    C = A@B
+    et = time.monotonic()
+    s = et-st
+
+    print(f"{flop/s * 1e-9:.2f}Gflops")
