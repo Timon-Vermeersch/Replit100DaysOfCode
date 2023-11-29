bb646543ad5f17c75f6b32eebf70f59d48df950b
YOLOTANKERSWAG2023 <timon.vermeersch@hotmail.com>
2023-11-29 23:43:00 +0100

    Finished .env login

--- 70.py
+++ 70.py
@@ -1,11 +1,13 @@
 #https://www.youtube.com/watch?v=Xrg2XP1JJec&list=PLto9KpJAqHMQNY3XP0JqLs7NyeU_dnNj0&index=117
 import dotenv
 import os
+import time
 
 dotenv_path = ".devcontainer/devcontainer.env"
 
 # Load environment variables from the specified file
 dotenv.load_dotenv(dotenv_path)
+
 
 
 username_admin = os.environ.get("USERNAME70ADMIN")
@@ -14,5 +16,28 @@ password_admin = os.environ.get("PASSWORD70ADMIN")
 username = os.environ.get("USERNAME70")
 password = os.environ.get("PASSWORD70")
 
-print(f"Admin username: {username_admin}, password: {password_admin}")
-print(f"Username: {username}, password: {password}")
+usernames = [username_admin,username]
+# print(f"Admin username: {username_admin}, password: {password_admin}")
+# print(f"Username: {username}, password: {password}")
+
+usrinp = ""
+
+while usrinp not in usernames:
+    print("--LOGIN SCREEN--")
+    usrinp = input("Please enter username: ")
+    if usrinp == username_admin:
+        adminPW = input(f"Please enter PW for {usrinp}: ")
+        if adminPW == password_admin:
+            print("Acces Granted")
+        else:
+            print("Acces denied")
+    elif usrinp == username:
+        usernamePW = input(f"Please enter PW for {usrinp}: ")
+        if usernamePW == password:
+            print("Acces Granted")
+        else:
+            print("Acces denied")
+    elif usrinp not in usernames:
+        print("Username not found")
+        time.sleep(1)
+        os.system("cls")
