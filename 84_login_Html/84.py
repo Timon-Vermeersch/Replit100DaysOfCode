5f5783902133e5ede14012c4188fda5c2525c808
YOLOTANKERSWAG2023 <timon.vermeersch@hotmail.com>
2023-12-12 23:28:26 +0100

    /welcome /gtfo /login /processlogin / processSignu

--- 84_login_Html/84.py
+++ 84_login_Html/84.py
@@ -17,7 +17,7 @@ def connect_to_database():
     return connection, cursor
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SESSION_PROTECTION'] = 'strong'
 @app.route("/" , methods = ['GET'])
@@ -39,25 +39,68 @@ def signup():
 def trail():
     return render_template(r"84_login_Html\templates\example.html")
 
-
 @app.route("/processSignup" , methods = ['POST'])
 def processSignup():
     connection,cursor = connect_to_database()
     wachtwoord = ''
     username = ''
     form = request.form
-    username = form['Username'] 
+    username = form['Username'].strip().lower()
     print(username)
     wachtwoord = form['Wachtwoord']
     data = (username , wachtwoord)
     print(data)
     try:
         cursor.execute(INSERT_SQL, data)
-    except: return "username already exists,niet perse though.nemen met een korrel zout"
+    except: return "username already exists,niet perse though. Te nemen met een korrel zout"
     connection.commit()
     
-    #flash('Thank you for registering')
-    #return redirect("/test") 
+    return redirect("/login") 
+
+@app.route("/processLogin" , methods = ['POST'])
+def processLogin():
+    connection,cursor = connect_to_database()
+    form = request.form
+    username = form['un'].strip().lower()
+    wachtwoord = form['pw']
+    
+    try: 
+        viewhashSQL = f"SELECT pw FROM Users where user = '{username}'"
+        cursor.execute(viewhashSQL)
+        uData = cursor.fetchone()
+        if wachtwoord == uData[0]:
+            return redirect("/welcome")
+        else: return redirect("/gtfo")
+    except:
+        return("Username prolly not found")
+    
+    
+    
+
+
+
+    data = username + wachtwoord
+    
+    return f"Welcome {username}"
+@app.route("/welcome")
+def welcome():
+    f = open("84_login_Html\welcome.html" , 'r' , encoding= "utf-8")
+    page = f.read()
+    f.close()
+    return page
+@app.route("/gtfo")
+def gtfo():
+    f = open("84_login_Html\gtfo.html" , 'r' , encoding= "utf-8")
+    page = f.read()
+    f.close()
+    return page
+@app.route("/login")
+def login():
+    f = open("84_login_Html\login.html" , 'r' , encoding= "utf-8")
+    page = f.read()
+    f.close()
+
+    return page
 
     
     
