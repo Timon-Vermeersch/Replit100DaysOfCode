bc2e19b9a336689494544e52538e2f581daff865
YOLOTANKERSWAG2023 <timon.vermeersch@hotmail.com>
2023-12-13 22:21:02 +0100

    pyramidBruteforce

---
+++ pyramid.py
@@ -0,0 +1,21 @@
+for i in range(0,10):
+    if i == 0:
+        print("                        1                        ")
+    elif i == 2:  
+        print("                     2  1  2                     ")
+    elif i == 3:
+        print("                  3  2  1  2  3                  ")
+    elif i == 4:
+        print("               4  3  2  1  2  3  4               ")
+    elif i == 5:
+        print("            5  4  3  2  1  2  3  4  5            ")
+    elif i == 6:
+        print("         6  5  4  3  2  1  2  3  4  5  6         ")
+    elif i == 7:
+        print("      7  6  5  4  3  2  1  2  3  4  5  6  7      ")
+    elif i == 8:
+        print("   8  7  6  5  4  3  2  1  2  3  4  5  6  7  8   ")
+    elif i == 9:
+        print("9  8  7  6  5  4  3  2  1  2  3  4  5  6  7  8  9")
+    
+
